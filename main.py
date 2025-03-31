372252a7e7d8a8e2733aa3431dbe7383852aae15
Lucas Baruffi <158331673+lucasbaruffi@users.noreply.github.com>
2025-03-31 10:02:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-import functions_framework
+from flask import Flask
+app = Flask(__name__)
 
-@functions_framework.http
-def app(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+@app.route('/')
+def hello():
+    return "Olá, App Engine!"
 
-    if request_json and 'name' in request_json:
-        name = request_json['name']
-    elif request_args and 'name' in request_args:
-        name = request_args['name']
-    else:
-        name = 'World'
-    return 'Hello {}!'.format(name)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
