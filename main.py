5ad4869b7ae34d94f07f02e89a238cef623c2799
Lucas Baruffi <158331673+lucasbaruffi@users.noreply.github.com>
2025-03-31 09:59:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import functions_framework
 
 @functions_framework.http
-def hello_http(request):
+def app(request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
